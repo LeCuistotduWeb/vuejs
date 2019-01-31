78d0470c95a0864695bf24190a7e21340ca46f40
Gaetan boyron <gaetan.boyron@nomadeducation.fr>
2019-01-31 17:51:01 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Liste en vueJs'
+    }
+});
+
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        text: 'Afficher',
+        btnMessage: `Afficher la liste ${new Date()}`,
+        todo: {
+            item :'',
+            item :'',
+            item :'',
+        },
+    },
+    methods: {
+        
+    },
+});
