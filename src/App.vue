973c39ffaf7a1c39e5133324ce7b1b48e58c35f2
LeCuistotduWeb <gaetan.boyron@gmail.com>
2019-02-01 15:14:08 +0100

    implement vue-cli

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="container-fluid text-center">
+      <div class="jumbotron">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1 class="h3">VueJs</h1>
+        </div>
+      <HelloWorld type="success" msg="Welcome to Your Vue.js App"/>
+    </div> 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todo-list">
+    <div class="alert alert-success">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
